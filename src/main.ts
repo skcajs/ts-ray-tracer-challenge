5da9ccb5a2c0c32fffcbba9fff9524d279a815f7
sam <sjackson864@outlook.com>
2024-08-18 14:08:31 +0200

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import './style.css'
 import Canvas from './canvas.ts'
-import spheres from "./scenes/spheres.ts";
-import {camera} from "./camera.ts";
 import {viewTransform} from "./maths/transformations.ts";
 import {point, vector} from "./maths/tuple.ts";
 
@@ -12,11 +10,17 @@ function main() {
         document.querySelector<HTMLCanvasElement>('#canvas')!,
         100, 50);
 
-    const scene = spheres();
+    const worker = new Worker(new URL('./worker.ts', import.meta.url));
 
-    const cam = camera(canvas.getWidth(), canvas.getHeight(), Math.PI / 3);
-    cam.transform = viewTransform(point(0, 1.5, -5), point(0, 1, 0), vector(0, 1, 0));
+    const transform = viewTransform(point(0, 1.5, -5), point(0, 1, 0), vector(0, 1, 0))
 
-    canvas.drawImage(cam.render(scene));
+    worker.postMessage({
+        width: canvas.getWidth(),
+        height: canvas.getHeight(),
+        fieldOfView: Math.PI / 3,
+        transform: transform.elements
+    });
+
+    worker.onmessage = (event) => canvas.drawImage(event.data);
 }
 
---
+++ src/worker.ts
@@ -0,0 +1,14 @@
+import {camera} from "./camera.ts";
+import spheres from "./scenes/spheres.ts";
+import Matrix from "./maths/matrix.ts";
+
+self.onmessage = (event) => {
+    const {width, height, fieldOfView, transform} = event.data;
+
+    const world = spheres();
+
+    const cam = camera(width, height, fieldOfView);
+    cam.transform = new Matrix(transform);
+
+    self.postMessage(cam.render(world));
+};
