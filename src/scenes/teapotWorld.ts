4945ace966924c7bd1fdfd18fa965e9eff827045
sam <sjackson864@outlook.com>
2024-09-02 12:35:46 +0200

    added teapot

--- src/scenes/teapotWorld.ts
+++ src/scenes/teapotWorld.ts
@@ -2,19 +2,20 @@ import {objToGroup, parseObjectFile} from "../shapes/parser.ts";
 import World, {emptyWorld} from "../world.ts";
 import PointLight from "../light.ts";
 import {makeColor, makePoint, makeVector} from "../tuple.ts";
-import {viewTransform} from "../transformations.ts";
+import {rotation, viewTransform} from "../transformations.ts";
 import teapot from "../test/data/teapot.txt?raw";
 import Matrix from "../matrix.ts";
 
 const teapotWorld = (): [World, Matrix] => {
     const parser = parseObjectFile(teapot);
-    const group = objToGroup(parser);
+    const teapotGroup = objToGroup(parser);
+    teapotGroup.setTransform(rotation(0, -Math.PI / 2).translate(0, -8, 0));
 
     const world = emptyWorld();
     world.light = new PointLight(makePoint(-10, 10, -10), makeColor(1, 1, 1));
-    world.objects = [group];
+    world.objects = [teapotGroup];
 
-    const transform = viewTransform(makePoint(-30, -30, -30), makePoint(0, 0, 0), makeVector(0, 1, 0));
+    const transform = viewTransform(makePoint(0, 0, -60), makePoint(0, 0, 0), makeVector(0, 1, 0));
 
     return [world, transform];
 };
